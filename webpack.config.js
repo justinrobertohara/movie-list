9506f9bf12525294fdc61f2a9be751ea6d1be977
Justin O'Hara <justinrobertohara@gmail.com>
2019-07-21 11:58:26 -0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
 const path = require('path');
 
 module.exports = {
+  //entry
+  //output
+  //mode
+  //module
+  // // rules
 
+  entry: './client/src/components/Index.jsx',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, './client/src/components')],
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
 };
